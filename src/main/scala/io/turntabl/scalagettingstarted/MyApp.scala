9c11104445a5716dc32f8277766c26069731e6d9
christianaasare <christiana.asare@turntabl.io>
2019-11-25 12:58:47 +0000

    Hello Scala

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -10,11 +10,28 @@ object MyApp extends App {
     def threeTimes(i:String)=i*3
     threeTimes("hello")
 
-    def even(i:Int)=i%2==0
+  def even(i:Int)= if (i%2==0)  true else false
 
-    def isWeekendDay(i:String)=i=="Saturday" | i=="Sunday"
+  def even2(i:Int)=i%2==0
 
-    def areWeekendDays(i:String*)=isWeekendDay("Saturday") | isWeekendDay("Sunday")
+  def odd2(i:Int) = !even(i)
+  def odd(i:Int) = if (!even(i)) true else false
+
+  def wordsToLower(words: String*)=
+    words map(_.toLowerCase)
+
+  def isWeekendDay(i:String)= i match{
+      case "saturday"|"sunday"=> true
+      case _ => false
+    }
+
+    def areWeekendDays(i:String*)= i map(isWeekendDay)
+  print(areWeekendDays("friday"))
+
+  def randomMethod(a:String, b:Int)=a*b
+
+  val result=areWeekendDays("monday","tuesday","wednesday","thursday", "friday")
+  result.foreach(println)
 
 
 }
