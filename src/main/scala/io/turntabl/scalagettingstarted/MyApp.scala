e50faeda7ba6a125a7405ec00bfc5cdd6b8fd646
christianaasare <christiana.asare@turntabl.io>
2019-11-25 11:17:02 +0000

    Scala 101

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -2,4 +2,19 @@ package io.turntabl.scalagettingstarted
 
 object MyApp extends App {
     print("Hello, World!")
+
+   val name: String = "Christiana"
+    val town: String = "Accra"
+    print( s"$name was born in $town")
+
+    def threeTimes(i:String)=i*3
+    threeTimes("hello")
+
+    def even(i:Int)=i%2==0
+
+    def isWeekendDay(i:String)=i=="Saturday" | i=="Sunday"
+
+    def areWeekendDays(i:String*)=isWeekendDay("Saturday") | isWeekendDay("Sunday")
+
+
 }
